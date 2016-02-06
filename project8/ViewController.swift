341c7c4339add1789da0912679c1d620e7a2fe4d
Alain Gall <alaingall@kl-pt-006.local>
2016-02-06 17:57:58 +0800

    Initial Commit

---
+++ project8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project8
+//
+//  Created by Alain Gall on 06/02/2016.
+//  Copyright © 2016 Alain Gall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
